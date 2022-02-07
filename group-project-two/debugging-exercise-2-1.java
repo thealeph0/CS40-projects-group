c943db9494cc69f3a06bf84f52ecd33b6ba09019
Kevin Alexis Giron <77304654+SIN88@users.noreply.github.com>
2022-02-06 17:19:59 -0800

    Update and rename debugging-exercise-2-1 to debugging-exercise-2-1.java

---
+++ group-project-two/debugging-exercise-2-1.java
@@ -0,0 +1,19 @@
+
+public class DebugTwo1
+{
+   public static void main(String[] args)
+   {//changed integer to int 
+      int oneInt = 315;
+      double oneDouble = 12.4;
+      // changed character to char
+      char oneChar = 'A';
+      System.out.print("The int is ");
+      //sytax error on call method 
+      System.out.println(oneInt);
+      System.out.print("The double is ");
+      //syntax error on call method
+      System.out.println(oneDouble);
+      System.out.print("The char is ");
+      System.out.println(oneChar);
+   }
+}
