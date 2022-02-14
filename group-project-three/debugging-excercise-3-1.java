63eea418374b2400a493fe6a3a7cdfee4d43314a
Kevin Alexis Giron <77304654+SIN88@users.noreply.github.com>
2022-02-14 15:28:24 -0800

    Update debugging-excercise-3-1.java

--- group-project-three/debugging-excercise-3-1.java
+++ group-project-three/debugging-excercise-3-1.java
@@ -1,1 +1,22 @@
 
+// This class calculates a waitperson's tip as 15% of the bill
+public class DebugThree1
+{
+   public static void main(String [] args)
+   {
+       /*syntax error check was lowercase*/
+      double myCheck = 50.00;
+      double yourCheck = 19.95;
+      System.out.println("Tips are");
+      calcTip(myCheck);
+      calcTip(yourCheck);
+    }//missing static in calcTip method
+    public static void calcTip(double bill)
+    {
+       final double RATE = 0.15;
+       double tip;
+       //symantics error , changed check to bill
+       tip = bill * RATE;
+       System.out.println("The tip should be at least " + tip);
+    }
+}
