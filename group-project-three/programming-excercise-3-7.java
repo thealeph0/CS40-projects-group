b947c84399bd5bb5798d25f7210e3595015db6a1
akdelgado <86629934+akdelgado@users.noreply.github.com>
2022-02-14 18:11:42 -0800

    Update programming-excercise-3-7.java

--- group-project-three/programming-excercise-3-7.java
+++ group-project-three/programming-excercise-3-7.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class InchConversion {
+    public static void main (String args[]) {
+        Scanner userInput=new Scanner(System.in);
+        System.out.print("Enter inches >>");
+        double inches=userInput.nextDouble();
+
+        double feet=convertToFeet(inches);
+        double yards=convertToYards(inches);
+
+        System.out.println(inches+" inches is "+feet+" feet");
+        System.out.println(inches+" inches is "+yards+" yards");
+    }
+
+    public static double convertToFeet(double inches) {
+        double feet=inches / 12;
+        return feet;
+    }
+    public static double convertToYards(double inches) {
+        double yards=inches / 36;
+        return yards;
+    }
+}
 
