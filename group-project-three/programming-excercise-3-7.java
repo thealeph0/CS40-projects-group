33b0baf6fe8708cf98d25ec75d118935dd333e11
thealeph0 <84290656+thealeph0@users.noreply.github.com>
2022-02-16 23:44:26 -0800

    Update programming-excercise-3-7.java
    Changed the methods so that the return value is a void so we output a string and the data type of the parameter is kept as a integer.

--- group-project-three/programming-excercise-3-7.java
+++ group-project-three/programming-excercise-3-7.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
-public class InchConversion {
+public class InchConversion 
+    {
     public static void main (String args[]) {
-        Scanner userInput=new Scanner(System.in);
-        System.out.print("Enter inches >>");
-        double inches=userInput.nextDouble();
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter inches >> ");
+        int inches = userInput.nextInt();
+        
+        convertToFeet(inches);
+        convertToYards(inches);
+    }
 
-        double feet=convertToFeet(inches);
-        double yards=convertToYards(inches);
-
+//Must keep as return void and int as input
+//in this method we do the calculations and store in apropriate variables
+    public static void convertToFeet(int inches) 
+    {
+        double inch = inches;
+        double feet = inch / 12;
         System.out.println(inches+" inches is "+feet+" feet");
-        System.out.println(inches+" inches is "+yards+" yards");
     }
 
-    public static double convertToFeet(double inches) {
-        double feet=inches / 12;
-        return feet;
-    }
-    public static double convertToYards(double inches) {
-        double yards=inches / 36;
-        return yards;
+//Must keep as return void and int as input
+//in this method we do the calculations in the ouput
+    public static void convertToYards(int inches) 
+    {
+        double inch = inches;
+        System.out.println(inches+" inches equals "+inch/36+" yards");
     }
 }
+
 
