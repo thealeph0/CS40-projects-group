18b7ce60f045027c4b8010ec688a71f86055be1e
akdelgado <86629934+akdelgado@users.noreply.github.com>
2022-03-03 01:00:54 -0800

    Update programming-excercise-4-7.java

--- group-project-four/programming-excercise-4-7.java
+++ group-project-four/programming-excercise-4-7.java
@@ -1,1 +1,23 @@
+public class MathTest {
+    public static void main(String[] args) {
+        // Write your code here
+        //number 1
+        System.out.println(Math.sqrt(37));
+        //number 2
+        System.out.println(Math.sin(300));
+        System.out.println(Math.cos(300));
+        //number 3
+        double x=22.8;
+        System.out.println(Math.floor(x)); //rounds down
+        System.out.println(Math.ceil(x)); //rounds up
+        System.out.println(Math.round(x)); //rounds to nearest whole number
+        //number 4
+        int y='D'; //integer value of character D
+        System.out.println(Math.max(y, 71));
+        System.out.println(Math.min(y, 71));
+        //number 5
+        double z= Math.random()*20; //gets rand # btwn 0 and 1 multiplied by 20
+        System.out.println(z);
 
+    }
+}
