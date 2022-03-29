bf7b1226d6215c949adca5bccc7257452edc1553
Poleron402 <89750832+Poleron402@users.noreply.github.com>
2022-03-29 09:40:07 -0700

    Update debugging-excercise-6-1.java

--- group-project-six/debugging-excercise-6-1.java
+++ group-project-six/debugging-excercise-6-1.java
@@ -1,1 +1,22 @@
-
+// Start with a penny
+// double it every day
+// how much do you have in a 30-day month?
+public class DebugSix1
+{
+   public static void main(String args[])
+   {
+      final int DAYS = 30;
+      double money = 0.01;
+      int day = 1;
+     //remove the semicolon after the while statement and change > to <
+      while(day < DAYS)
+      {
+         money = 2 * money;
+        //we do not have 'days' variable, but we do have day
+         ++day;
+        //moneyAmt does not exist, money does. 
+         System.out.println("After day " + day +
+            " you have " + money);
+      }
+   }
+}
