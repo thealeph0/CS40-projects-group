23d5d90cc2b90b73ecedd6c496d1316cdeb34917
akdelgado <86629934+akdelgado@users.noreply.github.com>
2022-04-18 12:27:22 -0700

    Update debugging-excercise-7-3.java

--- group-project-seven/debugging-excercise-7-3.java
+++ group-project-seven/debugging-excercise-7-3.java
@@ -1,1 +1,16 @@
+// Program displays some facts about a string
+public class DebugSeven3 //fixed spacing error
+{
+   public static void main(String[] args)
+   { 
+      String quote =
+         "Honesty is the first chapter in the book of wisdom. - Thomas Jefferson";
+      System.out.println("index.of('f') is: " + quote.indexOf('f')); //added .
+      System.out.println("index.of('x') is: " + quote.indexOf('x')); //added .
+      System.out.println("char.At(5) is: " + quote.charAt(5));
+      System.out.println("endsWith(\"daughter\") is: " + quote.endsWith("daughter"));
+      System.out.println("endsWith(\"son\") is: " + quote.endsWith("son"));
+      System.out.println("replace('e', '*') is: " + quote.replace('e', '*')); //replaced , with +
+   }
+}
 
